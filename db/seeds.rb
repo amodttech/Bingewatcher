51ae2bb8420a0e13aed4aef9944da2c033d11a72
Amodttech <aaron@amodttech.com>
2020-11-09 22:53:30 -0500

    seed cleanup

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,19 +26,13 @@ User.destroy_all
 Movielist.destroy_all
 # AddMovieToMovielist.destroy_all
 
-m1 = Movie.create(:title=>'Deep Red', :director=>'Dario Argento', :year=>1975, :genre=>'Horror', :country=>'Italy', :synopsis=>'A pianist investigates a series of murders performed by a mysterious figure wearing black leather gloves.')
-m2 = Movie.create(:title=>'Phantom of the Paradise', :director=>'Brian de Palma', :year=>1974, :genre=>'Horror', :country=>'USA', :synopsis=>'A pianists lifes work is stolen by a record tycoon, and he murders everyone who plays it.')
-m3 = Movie.create(:title=>'Saving Private Ryan', :director=> 'Steven Spielberg', :year=>1998, :genre=>'Action', :country =>'USA', :synopsis=>'saving ryan from the war')
-m4 = Movie.create(:title=>'Titanic', :director=>'James Cameron', :year=>1997, :genre=>'Drama', :country=>'USA', :synopsis=>'boat goes down...')
-
-# u1 = User.create(:name=>'Aaron')
-# u2 = User.create(:name=>'Julio')
-
-# # r1 = Review.create(u1, m1, 4)
-# # r2 = Review.create(u1, m2, 2)
-# # r3 = Review.create(u1, m3, 9)
-# # r4 = Review.create(u2, m1, 2)
-# # r5 = Review.create(u2, m3, 6)
-# # r6 = Review.create(u2, m4, 10)
+Movie.create(title: 'Deep Red', director: 'Dario Argento', year: 1975, genre: 'Horror', country: 'Italy', synopsis: 'A pianist investigates a series of murders performed by a mysterious figure wearing black leather gloves.')
+Movie.create(title: 'Phantom of the Paradise', director: 'Brian de Palma', year: 1974, genre: 'Horror', country: 'USA', synopsis: 'A pianists lifes work is stolen by a record tycoon, and he murders everyone who plays it.')
+Movie.create(title: 'Saving Private Ryan', director:  'Steven Spielberg', year: 1998, genre: 'Action', country:  'USA', synopsis: 'saving ryan from the war')
+Movie.create(title: 'Titanic', director: 'James Cameron', year: 1997, genre: 'Drama', country: 'USA', synopsis: 'boat goes down...')
 
 
+User.create(name: 'Aaron')
+User.create(name: 'Julio')
+
+Review.create(user_id: u1, movie_id: m1, rating: 3)
